e8f24e2387c7984007780ccc6ce3044b2e8f9302
Abdullah Amjad <shkabdullah268@gmail.com>
2025-11-02 04:16:38 -0800

    Adding PreLaoder

---
+++ src/components/Preloader.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+import { useRef } from "react";
+
+gsap.registerPlugin(SplitText);
+
+export default function Preloader() {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(() => {
+    const el = wrapperRef.current;
+    if (!el) return;
+
+    const split = new SplitText("#preloader-title", { type: "chars" });
+    gsap.from(split.chars, {
+      duration: 1,
+      yPercent: 40,
+      opacity: 0,
+      ease: "expo.out",
+      stagger: 0.05,
+      onComplete: () => {
+        gsap.to(el, {
+          y: "-100vh",
+          duration: 0.8,
+          ease: "power3.inOut",
+          onComplete: () => el.remove(),
+        });
+      },
+    });
+  }, []);
+
+  return (
+    <div
+      ref={wrapperRef}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-[#F1F2F2]"
+    >
+      <h1
+        id="preloader-title"
+        className="text-7xl font-bold tracking-wide text-center"
+      >
+        Welcome to&nbsp;
+        <span className="text-[#1a3501]">Anima</span>
+        <br />
+        <span className="text-[#6e3000]">The Revolution</span>
+      </h1>
+    </div>
+  );
+}
